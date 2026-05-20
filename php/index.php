24b990681d4b38d9d027c7c28dc954d138587887
student <student@itismeucci.com>
2026-05-20 10:43:10 +0200

    inizializzazione rotte

--- php/index.php
+++ php/index.php
@@ -9,29 +9,9 @@ require __DIR__ . '/controllers/AlunniController.php';
 
 $app = AppFactory::create();
 
-$app->get('/test', function (Request $request, Response $response, array $args) {
-    $response->getBody()->write("Test page");
-    return $response;
-});
 
-$app->get('/up', function (Request $request, Response $response, array $args) {
-    $response->getBody()->write("OK");
-    return $response;
-});
 
-$app->get('/', function (Request $request, Response $response, array $args) {
-    $payload = json_encode(['status' => 'ok']);
-    $response->getBody()->write($payload);
-    return $response->withHeader("Content-type", "application/json");
-});
 
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    return $response;
-});
-
-$app->get('/alunni', "AlunniController:index");
-$app->get('/api/alunni', "AlunniController:index");
+$app->get('/account/{id}', "AccountController:index");
 
 $app->run();
